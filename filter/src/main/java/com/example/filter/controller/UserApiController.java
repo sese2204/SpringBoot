6551ab7048ad23302bd5c40219852a1982b0cf87
sese2204 <sese2204@naver.com>
2024-02-02 00:42:02 +0900

    filter

---
+++ filter/src/main/java/com/example/filter/controller/UserApiController.java
@@ -0,0 +1,24 @@
+package com.example.filter.controller;
+
+import com.example.filter.model.UserRequest;
+import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+@Slf4j
+@Builder
+public class UserApiController {
+    //1232121
+    @PostMapping("")
+    public void register(
+        @RequestBody
+        UserRequest userRequest
+    ){
+        log.info("{}", userRequest);
+    }
+}
