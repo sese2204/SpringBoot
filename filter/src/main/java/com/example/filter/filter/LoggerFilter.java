01aa5822d11a0dbd40bbc3ed550317fb1c3b0826
sese2204 <sese2204@naver.com>
2024-02-02 01:30:30 +0900

    commit

---
+++ filter/src/main/java/com/example/filter/filter/LoggerFilter.java
@@ -0,0 +1,22 @@
+package com.example.filter.filter;
+
+
+import jakarta.servlet.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+@Slf4j
+@Component
+public class LoggerFilter implements Filter {
+    //123
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        log.info(">>>>> 진입");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+
+        log.info("<<<<<< 리턴");
+    }
+}
